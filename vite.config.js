4381ddd4044ffa5d3313adb1aee2a06fe3e43203
Yoanna <ioannaiordanova99@gmail.com>
2026-02-11 22:08:14 +0200

    02.11.2026

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin()
+  ],
+  build: {
+    cssCodeSplit: false
+  }
 })
+
